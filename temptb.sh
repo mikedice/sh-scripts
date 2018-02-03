e9d21551f816044414967e08564b18ea04a309df
Mike Dice <mikedice417@hotmail.com>
2018-02-02 20:52:11 -0800

    Fix Cleber's comment

--- temptb.sh
+++ temptb.sh
@@ -5,6 +5,6 @@
 #
 cputemp=$(</sys/class/thermal/thermal_zone1/temp)
 gputemp=$(</sys/class/thermal/thermal_zone2/temp)
-echo "CPU: " $(echo "scale=2; ($cputemp/1000) * (9/5) + 32" |bc)
-echo "GPU: " $(echo "scale=2; ($gputemp/1000) * (9/5) + 32" |bc)
+echo "CPU: " $(echo "scale=2; ($cputemp/1000) * (9/5) + 32" |bc) "F"
+echo "GPU: " $(echo "scale=2; ($gputemp/1000) * (9/5) + 32" |bc) "F"
 
